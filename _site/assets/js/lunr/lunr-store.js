1c76bb4c001e5acdbd3fd5e06bb47178b44f4cfe
Andre Maia Chagas <andremaia.chagas@gmail.com>
2024-04-18 06:32:11 +0100

    add github actions to local copy and add _site to things to be pushed to GH

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "ORTHUS launch event",
+        "excerpt":"Welcome   Hello world, this is my first Jekyll blog post.   I hope you like it!  ","categories": [],
+        "tags": [],
+        "url": "/ORTHUS_launch/",
+        "teaser": null
+      }]
